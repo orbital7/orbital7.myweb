44bd93010dd50ba18bb2378fc70deca0e29d79bc
Josh van Eikeren <josh@vaneikeren.com>
2019-02-22 11:10:45 -0800

    WebJob first commit

---
+++ src/Apps/WebJob/Program.cs
@@ -0,0 +1,22 @@
+﻿using Orbital7.MyWeb.Services.Default;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace WebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = DefaultServicesFactory.CreateDefault();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            var temp = configuration["StorageConnectionString"];
+            Console.WriteLine("Test: " + temp);
+        }
+    }
+}
