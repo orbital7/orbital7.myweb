271d95d7ec408797cae413c7c4326cf59d732a1d
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2020-06-14 17:18:27 -0700

    Test commit under new branch name

--- src/Apps/WebApp/Program.cs
+++ src/Apps/WebApp/Program.cs
@@ -18,7 +18,6 @@ namespace WebApp
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
