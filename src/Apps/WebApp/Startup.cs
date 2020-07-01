5316e7442b152ff2872d926cf98a859fb13c2ea8
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2020-07-01 10:13:33 -0700

    Updated to RapidApp dynamic layout

--- src/Apps/WebApp/Startup.cs
+++ src/Apps/WebApp/Startup.cs
@@ -24,12 +24,17 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews()
+            services.AddMyWebDefaultServices();
+
+            var builder = services.AddControllersWithViews()
                     .AddNewtonsoftJson(options =>
                     {
                         options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Error;
                     });
-            services.AddMyWebDefaultServices();
+
+            #if debug
+                builder.AddRazorRuntimeCompilation();
+            #endif
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
